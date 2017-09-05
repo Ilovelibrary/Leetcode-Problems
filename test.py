03c5ca194f85af1f10a319f0afb57e755fe115d0
Pu <p5gao@ucsd.edu>
2017-09-04 23:55:44 -0700

    215

---
+++ test.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def findKthLargest(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        heap = []
+        for num in nums:
+            heapq.heappush(heap, -num)
+        for i in range(k-1):
+            heapq.heappop(heap)
+        return -heapq.heappop(heap)
